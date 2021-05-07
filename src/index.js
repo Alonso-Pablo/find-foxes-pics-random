620960e63c4dfd3f62ac014a392f0713b641b2f0
Alonso-Pablo <sidokunoice@gmail.com>
2021-05-07 15:59:54 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,82 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+/*
+const fox = document.getElementById("fox")
+const apiURL = "https://randomfox.ca/floof/";
+console.log("Hola")
+let numberImgToShow = 10;
 
-console.log('Happy hacking :)')
+const response = async (nImages) => {
+    const urlSave = [];
+    const elImg = [];
+    
+    // Fetch
+    for (let i = 0; i<nImages; i++){
+        try {
+            const responseFetch = await fetch(apiURL);
+            const data = await responseFetch.json();
+            urlSave.push(data.image)
+        } catch (error){console.log(error.message)}
+    };
+
+    // Create html el
+    urlSave.forEach((image) => {
+        const img = document.createElement('img');
+        img.classList.add("mx-auto")
+        img.style = 'max-width: 320px';
+        img.src = image;
+
+        const container = document.createElement('div')
+        container.classList.add("p-4")
+        container.append(img);
+
+        elImg.push(container)
+        console.log(image);
+    });
+
+    fox.append(...elImg);
+
+}
+
+    
+    
+    // const img = document.createElement('img');
+
+response(numberImgToShow);
+
+*/
+// Como lo hizo el profesor:
+import { registerImage } from './lazy'
+
+export const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = "p-4 div-fox";
+
+    const imagen = document.createElement('img')
+    imagen.className = "loading bg-gray-100";
+    imagen.width = '320'
+    // imagen.src = `https:\/\/randomfox.ca\/images\/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const imagesContainer = document.getElementById('images-container');
+
+const addButton = document.getElementById('addButton');
+const deleteButton = document.getElementById('deleteButton');
+
+const addImage = () => {
+    const newImage = createImageNode();
+    imagesContainer.append(newImage);
+    // quiero que la registres en el lazy loading y la empiezes a escuchar 
+    registerImage(newImage);
+}
+
+function deleteImages() {
+while (imagesContainer.firstChild){
+    imagesContainer.removeChild(imagesContainer.firstChild)
+}}
+
+addButton.addEventListener('click', addImage)
+deleteButton.addEventListener('click', deleteImages)
+
---
+++ src/lazy.js
@@ -0,0 +1,40 @@
+import { createImageNode } from './index'
+
+const minimum = 1;
+const maximum = 122;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum
+
+
+const isIntersecting = (entry) => {
+    // Tambien podemos condicionar si esta a 200px lejos de la pantalla --do--> something
+    return entry.isIntersecting // True si esta dentro de la pantalla o false si no esta dentro.
+}
+const loadImage = (entry) => {
+    const nodo = entry.target
+    // Accion que sucede cuando se encuentra dentro del viewport
+    // Esta accion sucede varias veces con la misma imagen cuando sale y entra denuevo asi que hay que registrarla
+    // Usamos un metodo unobserve(nodo)
+    // Como parte de la API, esta accion que es ejecutada por el IntersectionObserver trae en su primer argumento el entry que en ese momento sucede.
+    observador.unobserve(nodo)
+    nodo.firstElementChild.src = `https:\/\/randomfox.ca\/images\/${random()}.jpg`;
+
+    // Genera una imagen al scrollear para abajo.
+    const imagesContainer = document.getElementById('images-container');
+    const newImage = createImageNode();
+    imagesContainer.append(newImage);
+    registerImage(newImage);
+}
+
+const observador = new IntersectionObserver((entries) => {
+    // El intersection observer va a ejecutar esta funcion y nos va a pasar todas las entradas que en ese momento está escuchando.
+    // entries es un array asi que lo vamos a filtrar.
+    // por cada elemento que haya en la pantalla vamos a hacer una accion
+    entries
+        .filter(isIntersecting)
+        .forEach(loadImage)
+})
+
+export const registerImage = (image) => {
+    // IntersectionObserver -> observar(image)
+    observador.observe(image)
+}
